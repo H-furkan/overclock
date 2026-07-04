434a135db11c6ebcdb20827869d11cbda53fac2d
H-furkan <furkanhoroz125@gmail.com>
2026-07-04 18:04:56 -0500

    v7.24.2: (1) Ideal calisma suresi SIFIRLA butonu (prodSessions temizler, onayli). (2) Pixel avatar profesyonellestirildi: otomatik kontur+isiklandirma motoru (_avComposite tampon grid + _avPaint: siluet cevresine 1px koyu kontur, ustten isik/alttan golge) - tum sprite'lar otomatik derinlik kazandi; kilic 2px genis + balta yeniden cizildi

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // OVERCLOCK service worker — offline cache + scheduled (background) notifications
-const CACHE = 'overclock-v7-24-1';
+const CACHE = 'overclock-v7-24-2';
 
 self.addEventListener('install', e => self.skipWaiting());
 
